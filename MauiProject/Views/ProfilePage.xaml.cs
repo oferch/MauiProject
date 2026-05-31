2e6e574a58792b1229dcea22efe5113052b5aa3d
oferc <ofer@whitenblue.com>
2026-05-31 21:26:23 +0300

    Add PRofile page including permissions for camera and contacts

---
+++ MauiProject/Views/ProfilePage.xaml.cs
@@ -0,0 +1,62 @@
+namespace MauiProject.Views;
+
+using Communication = Microsoft.Maui.ApplicationModel.Communication;
+
+
+public partial class ProfilePage : ContentPage
+{
+	public ProfilePage()
+	{
+		InitializeComponent();
+
+    }
+
+    private async void OnFetchContactClicked(object sender, EventArgs e)
+    {
+        var status = await Permissions.CheckStatusAsync<Permissions.ContactsRead>();
+
+        if (status != PermissionStatus.Granted)
+        {
+            status = await Permissions.RequestAsync<Permissions.ContactsRead>();
+        }
+
+        if (status == PermissionStatus.Granted)
+        {
+            try
+            {
+                var contact = await Communication.Contacts.Default.PickContactAsync();
+
+                if (contact == null)
+                    return;
+
+                string id = contact.Id;
+                string namePrefix = contact.NamePrefix;
+                string givenName = contact.GivenName;
+                string middleName = contact.MiddleName;
+                string familyName = contact.FamilyName;
+                string nameSuffix = contact.NameSuffix;
+                string displayName = contact.DisplayName;
+                List<ContactPhone> phones = contact.Phones; // List of phone numbers
+                List<ContactEmail> emails = contact.Emails; // List of email addresses
+            }
+            catch (Exception ex)
+            {
+                // Most likely permission denied
+            }
+        }
+        else
+        {
+            await Shell.Current.DisplayAlertAsync("Permission Denied", "Unable to access contacts. Please grant permission and try again.", "OK");
+        }
+
+        
+
+    }
+
+
+
+    private async Task OnChangeClickedAsync(object sender, TappedEventArgs e)
+    {
+            var cameraPermissionsRequest = await Permissions.RequestAsync<Permissions.Camera>();
+    }
+}
