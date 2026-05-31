5664b95ccb63a7115e3be440517b660e2cac075d
oferc <ofer@whitenblue.com>
2026-05-31 21:26:55 +0300

    Add Store page infrastructure

---
+++ MauiProject/Models/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiProject.Models
+{
+
+    internal class Product : ObservableObject
+    {
+        private double price=0;
+        private string name="";
+        private string imageUrl="";
+        private bool isFaovrite=false;
+
+        public string Name { get => name; 
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public double Price { get => price; 
+            set
+            {
+                if (price != value)
+                {
+                    price = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string ImageUrl { get => imageUrl;
+            set
+            {
+                if (imageUrl != value)
+                {
+                    imageUrl = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public bool IsFavorite { get => isFaovrite;
+            set
+            {
+                if (isFaovrite != value)
+                {
+                    isFaovrite = value;
+                    OnPropertyChanged();
+                }       
+            }
+        }
+    }
+}
---
+++ MauiProject/Services/SqliteStore.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiProject.Services
+{
+    internal class SqliteStore
+    {
+    }
+}
